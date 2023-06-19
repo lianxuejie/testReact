633b7a0dae09f866f01ea8d573ec6eda05f059e6
xiayu <xiayu@newborntown.com>
2023-06-19 19:23:11 +0800

    fix module css

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import './style.less'
+import testStyle from './testStyle.module.css'
 
 const LoginPage = () => {
   return (
     <div>
       <h2 className='red-font'>login async</h2>
-      <div className='red-font'>
+      <div className={testStyle.title}>
         test red color
       </div>
     </div>
